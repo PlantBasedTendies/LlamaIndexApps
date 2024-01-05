2f353479fc4b594b7adafb0421aa7b917a78ad29
PlantBasedTendies <86295293+PlantBasedTendies@users.noreply.github.com>
2024-01-05 14:58:58 -0500

    Add files via upload

--- streamlit_hawking_openai.py
+++ streamlit_hawking_openai.py
@@ -6,7 +6,7 @@ Overview:
     file containing a history of Stephen Hawking to ask questions about one of
     the world's greatest astrophysicists.
 
-    Note: An OpenAI key is necessary for this version of the script.
+    **Note: An OpenAI key is necessary for this version of the script.**
 
     Date: 1/05/2024
 
@@ -24,19 +24,23 @@ open_api_key = os.getenv("OPENAI_API_KEY")
 st.title("Stephen Hawking Q&A :telescope: ")
 
 intro = '''**Examples:**  
-:pill: :orange[What illness did Stephen Hawking develop?]  
-:bald_man: :green[What was Stephen Hawking's father's name?]  
-:boom: :blue[What kind of work helped Stephen Hawking prove the idea of the 'Big Bang'?]
-'''
+        :pill: :orange[What illness did Stephen Hawking develop?]  
+        :bald_man: :green[What was Stephen Hawking's father's name?]  
+        :boom: :blue[What kind of work helped Stephen Hawking prove the idea of the 'Big Bang'?]
+        '''
 st.markdown(intro)
 
 user_input = st.text_input("Ask a question about Stephen Hawking:")
 
+# LlamaIndex integrated RAG
 # text file resides in /hawking_data/
-documents = SimpleDirectoryReader("hawking_data").load_data()
+documents = SimpleDirectoryReader("hawking_data").load_data() 
+
 index = VectorStoreIndex.from_documents(documents)
+
 query_engine = index.as_query_engine()
 
+# Check for input and produce output
 if user_input:
     response = query_engine.query(user_input)
     response_string = str(response)

1cbb3ca4a3d270b822f7cf82e336ac77228831dc
PlantBasedTendies <86295293+PlantBasedTendies@users.noreply.github.com>
2024-01-05 15:49:17 -0500

    Add files via upload

--- streamlit_hawking_openai.py
+++ streamlit_hawking_openai.py
@@ -23,28 +23,23 @@ open_api_key = os.getenv("OPENAI_API_KEY")
 
 # Streamlit app layout
 st.title("Stephen Hawking Q&A :telescope:")
-
 intro = '''**Examples:**  
-        :pill: :orange[What illness did Stephen Hawking develop?]  
-        :bald_man: :green[What was Stephen Hawking's father's name?]  
-        :boom: :blue[What kind of work helped Stephen Hawking prove the idea of the 'Big Bang'?]
+           :pill:     :orange[What illness did Stephen Hawking develop?]  
+           :bald_man: :green[What was Stephen Hawking's father's name?]  
+           :boom:     :blue[What kind of work helped Stephen Hawking prove the idea of the 'Big Bang'?]
         '''
 st.markdown(intro)
-
 user_input = st.text_input("Ask a question about Stephen Hawking:")
 
 # LlamaIndex integrated RAG
 # text file resides in /hawking_data/
 documents = SimpleDirectoryReader("hawking_data").load_data() 
-
 index = VectorStoreIndex.from_documents(documents)
-
 query_engine = index.as_query_engine()
 
 # Check for input and produce output
 if user_input:
     response = query_engine.query(user_input)
     response_string = str(response)
-
     # Display the output
     st.write(response_string)
